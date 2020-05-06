ef640841a632e09402018d82c752814623c5152b
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-05-05 20:38:30 -0400

    Update ionomy.py

--- src/Ionomy/ionomy.py
+++ src/Ionomy/ionomy.py
@@ -1,40 +1,56 @@
 import hashlib
 import hmac
 import json
-from typing import Any, Dict, List, Optional, Union
-
 import arrow
 import requests
+
 from furl import furl
 from requests import Session
+from typing import Any, Dict, List, Optional, Union, Callable, Any
 
-class Ionomy(object):
-    IONOMY_URL_V1 = 'https://ionomy.com/api/v1/'
+from .ticker import Ticker
+from .utils.decorators import currency_to_crypto
 
-    def __init__(self, api_key: str, api_secret: str) -> None:
-        self.api_calls = 0
-        
-        self._api_key = api_key
-        self._api_secret = api_secret.encode('utf-8')
+class Ionomy(Ticker):
+    _ion_client: Session = requests.Session()
 
-        self._ion_client: Session = requests.Session()
+    def __init__(
+        self,
+        ion_api_key: str,
+        ion_api_secret: str,
+        cc_api_key: str = "",
+        crypto: str = "",
+        base: str = "",
+        time: str = "",
+        exchange: str = "",
+        market: str = ""
+    ) -> None:
+        Ticker.__init__(self, cc_api_key, crypto, base, time, exchange, market)
+        self.ion_api_key = ion_api_key
+        self.ion_api_secret = ion_api_secret
         self.market_names = [data["market"] for data in self._ion_request('public/markets')]
     
     def _get_signature(self, endpoint: str, params: dict, timestamp: str) -> str:
-        api_furl = furl(self.IONOMY_URL_V1 + endpoint)
+        api_furl = furl('https://ionomy.com/api/v1/' + endpoint)
         api_furl.args = params
         url_ts = (api_furl.url + timestamp).encode('utf-8')
-        return hmac.new(self._api_secret, url_ts, hashlib.sha512).hexdigest()
+        return hmac.new(
+            self.ion_api_secret.encode('utf-8'),
+            url_ts, hashlib.sha512
+        ).hexdigest()
 
-    def _ion_request(self, endpoint: str, params: dict={}):
+    def _ion_request(self, endpoint: str, params: dict={}) -> Any:
         timestamp = str(arrow.utcnow().timestamp)
         headers = {
             'api-auth-time': timestamp,
-            'api-auth-key': self._api_key,
+            'api-auth-key': self.ion_api_key,
             'api-auth-token': self._get_signature(endpoint, params, timestamp)
         }
-        self.api_calls += 1
-        response = self._ion_client.get(self.IONOMY_URL_V1 + endpoint, params=params, headers=headers)
+        response = self._ion_client.get(
+            'https://ionomy.com/api/v1/' + endpoint,
+            params=params,
+            headers=headers
+        )
         data = json.loads(response.content)
         if not data['success']:
             raise Exception(data['message'])
@@ -43,41 +59,55 @@ class Ionomy(object):
     def markets(self) -> List[Dict[str, Any]]:
         return self._ion_request('public/markets')
 
-    def currencies(self) -> list:
+    @currency_to_crypto()
+    def cryptos(self) -> list:
         return self._ion_request('public/currencies')
         
-    def order_book(self, market: str) -> dict:
-        return self._ion_request('public/orderbook', {'market': market, 'type': 'both'})
+    def order_book(self, market: str = "") -> dict:
+        return self._ion_request(
+            'public/orderbook',
+            {
+                'market': self._cfg(market, "market"),
+                'type': 'both'
+            }
+        )
 
     def market_summaries(self) -> list:
         return self._ion_request('public/markets-summaries')
 
-    def market_summary(self, market: str) -> dict:
-        return self._ion_request('public/market-summary', {'market': market})
+    def market_summary(self, market: str = "") -> dict:
+        return self._ion_request(
+            'public/market-summary',
+            {'market': self._cfg(market, "market")}
+        )
 
-    def market_history(self, market: str) -> list:
-        return self._ion_request('public/market-history', {'market': market})
+    def market_history(self, market: str = "") -> list:
+        return self._ion_request(
+            'public/market-history',
+            {'market': self._cfg(market, "market")}
+        )
 
     def limit_buy(
         self,
-        market: str,
         amount: Union[int, float],
-        price: Union[int, float]
+        price: Union[int, float],
+        market: str = "",
     ) -> dict:
         params = {
-            'market': market,
+            'market': self._cfg(market, "market"),
             'amount': f'{amount:.8f}',
             'price': f'{price:.8f}'
         }
         return self._ion_request('market/buy-limit', params)
     
     def limit_sell(
-        self, market: str,
+        self,
         amount: Union[int, float],
-        price: Union[int, float]
+        price: Union[int, float],
+        market: str = ""
     ) -> dict:
         params = {
-            'market': market,
+            'market': self._cfg(market, "market"),
             'amount': f'{amount:.8f}',
             'price': f'{price:.8f}'
         }
@@ -87,23 +117,43 @@ class Ionomy(object):
         self._ion_request('market/cancel-order', {'orderId': orderId})
         return True
 
-    def open_orders(self, market: str) -> list:
-        return self._ion_request('market/open-orders', {'market': market})
+    def open_orders(self, market: str = "") -> list:
+        return self._ion_request(
+            'market/open-orders',
+            {'market': self._cfg(market, "market")}
+        )
 
+    @currency_to_crypto()
     def balances(self) -> list:
         return self._ion_request('account/balances')
 
-    def balance(self, currency: str) -> dict:
-        return self._ion_request('account/balance', {'currency': currency})
+    @currency_to_crypto()
+    def balance(self, currency: str = "") -> dict:
+        return self._ion_request(
+            'account/balance',
+            {'currency': self._cfg(currency, "currency")}
+        )
 
-    def deposit_address(self, currency: str) -> dict:
-        return self._ion_request('account/deposit-address', {'currency': currency})
+    @currency_to_crypto()
+    def deposit_address(self, currency: str = "") -> dict:
+        return self._ion_request(
+            'account/deposit-address',
+            {'currency': self._cfg(currency, "currency")}
+        )
 
-    def deposit_history(self, currency: str) -> list:
-        return self._ion_request('account/deposit-history', {'currency': currency})
+    @currency_to_crypto()
+    def deposit_history(self, currency: str = "") -> list:
+        return self._ion_request(
+            'account/deposit-history',
+            {'currency': self._cfg(currency, "currency")}
+        )
 
-    def withdrawal_history(self, currency: str) -> dict:
-        return self._ion_request('account/withdrawal-history', {'currency': currency})
+    @currency_to_crypto()
+    def withdrawal_history(self, currency: str = "") -> dict:
+        return self._ion_request(
+            'account/withdrawal-history',
+            {'currency': self._cfg(currency, "currency")}
+        )
 
     def get_order_status(self, orderId: str) -> dict:
         return self._ion_request('account/order', {'orderId': orderId})
