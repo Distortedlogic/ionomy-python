f839a20bd1f9273418f58ddf3e2928e129443915
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2020-05-05 20:37:36 -0400

    Update __init__.py

--- src/Ionomy/__init__.py
+++ src/Ionomy/__init__.py
@@ -1,5 +1,4 @@
 from .ionomy import Ionomy
 from .ion_panda import IonPanda
-from .cc_ticker import CCTicker
-from .cmc_ticker import CMCTicker
+from .ticker import Ticker
 from .ion_ta import IonTA
