be806a2a678a3b6bf6650e3933fcbe824b040c00
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-04-24 08:42:28 -0400

    save

--- setup.py
+++ setup.py
@@ -3,16 +3,9 @@ from setuptools import find_namespace_packages, setup
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.0.6'
+VERSION = "0.0.7"
 
-requires = [
-    'requests',
-    'arrow',
-    'furl',
-    'python-decouple',
-    'pandas',
-    'pandas-ta'
-]
+requires = ["requests", "arrow", "furl", "python-decouple", "pandas", "pandas-ta"]
 dev_requires = {}
 
 entry_points = """
@@ -21,29 +14,29 @@ entry_points = """
 """
 
 setup(
-    name='ionomy-python',
+    name="ionomy-python",
     version=VERSION,
-    author='DistortedLogic/Memehub',
-    author_email='jermeek@gmail.com',
-    url='https://github.com/Distortedlogic/ionomy-python',
-    license='LICENSE.txt',
-    description='',
+    author="DistortedLogic/Memehub",
+    author_email="jermeek@gmail.com",
+    url="https://github.com/Distortedlogic/ionomy-python",
+    license="LICENSE.txt",
+    description="",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    python_requires='>=3.6',
-    packages=find_namespace_packages(where='src', exclude=['docs', 'tests*']),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    packages=find_namespace_packages(where="src", exclude=["docs", "tests*"]),
     install_requires=requires,
     extras_require=dev_requires,
-    package_dir={'': 'src'},
-    keywords=['hive', 'ionomy', 'library', 'api', 'rpc'],
+    package_dir={"": "src"},
+    keywords=["hive", "ionomy", "library", "api", "rpc"],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Intended Audience :: Developers",
     ],
     entry_points=entry_points,
-    include_package_data=True
+    include_package_data=True,
 )
